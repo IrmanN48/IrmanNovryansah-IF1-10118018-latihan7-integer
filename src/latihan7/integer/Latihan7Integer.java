cea2988c585a45c38592839bac77480a5f8c50dc
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 22:46:10 +0700

    Add files via upload

---
+++ src/latihan7/integer/Latihan7Integer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan7.integer;
+
+/**
+ *
+ * @author Aero
+ * NAMA     : Irman Novryansah
+ * KELAS    : IF-3
+ * NIM      : 10117111
+ * Deskripsi Program : Program ini untuk menampilkan value integer yang sudah 
+ *                      ditentukan
+ */
+public class Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        byte b;
+        short s;
+        int i;
+        long I;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        I = 90000000000L;
+        System.out.println("byte: : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + I);
+        
+        
+    }
+    
+}
